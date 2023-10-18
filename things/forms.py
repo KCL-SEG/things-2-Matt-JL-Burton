172cad21e79f3a2136191664a5ae2c983cff94aa
Matt-JL-Burton <mslburton24@yahoo.com>
2023-10-18 15:53:09 +0100

    Add the ThingFrom with a meta class data
    Also added the form templat

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
+from django import forms
 
-# Create your forms here.
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea
+        }
+
+    # name = forms.CharField()
+    # description = forms.CharField()
+    # quantity = forms.IntegerField(min_value=0, max_value=50)
